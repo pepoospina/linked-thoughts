4eafcf67a4e42fe46b0299b3ce38c2a11a42d6a3
Pepo <pepo@uprtcl.io>
2021-02-12 19:22:38 +0100

    meeting

--- src/services/app.elements.patterns.ts
+++ src/services/app.elements.patterns.ts
@@ -1,10 +1,12 @@
+import { TextNodePattern } from '@uprtcl/documents';
 import { Pattern, HasChildren } from '@uprtcl/evees';
 
-import { Dashboard, Section } from '../containers/types';
+import { Dashboard, LT_TextNode, Section } from '../containers/types';
 
 export const DashboardType = 'LinkedThoughts:Dashboard';
 export const SectionType = 'LinkedThoughts:Section';
 export const HomeType = 'LinkedThoughts:UserHome';
+export const LT_TextNodeType = 'LinkedThoughts:LT_TextNode';
 
 export class AppHomePattern extends Pattern<any> {
   recognize(object: any): boolean {
@@ -77,4 +79,30 @@ export class SectionBehaviors implements HasChildren<Section> {
   });
 
   children = (node: Section): string[] => node.pages;
+}
+
+export class LT_TextNodePattern extends TextNodePattern {
+  recognize(object: any): boolean {
+    return (
+      super.recognize(object) &&
+      object.meta !== undefined &&
+      object.meta.isA !== undefined
+    );
+  }
+
+  type = LT_TextNodeType;
+
+  constructor() {
+    super([new LT_TextNodeBehaviors()]);
+  }
+}
+
+export class LT_TextNodeBehaviors implements HasLinks<LT_TextNode> {
+  linksTo = async (node: LT_TextNode) => node.meta.isA;
+  replaceLinksTo = (node: LT_TextNode) => (links: string[]): LT_TextNode => ({
+    ...node,
+    meta: {
+      isA: links,
+    },
+  });
 }
